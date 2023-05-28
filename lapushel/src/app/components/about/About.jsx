4e9596ca3eda8602a4156944b5a6d4a00498b13e
La-Pucelle <108086683+La-Pucelle@users.noreply.github.com>
2023-05-27 22:14:23 -0400

    commit

--- lapushel/src/app/components/about/About.jsx
+++ lapushel/src/app/components/about/About.jsx
@@ -2,9 +2,9 @@ import styles from './About.module.css'
 
 const item = [
     {
-        
+        tittle: 'hola',
         description: 'Throughout my academic journey, I have been immersed in the world of software development, gaining a solid foundation in programming principles and techniques. From learning essential programming languages to exploring data structures and algorithms, I have been honing my skills to become a proficient software engineer.'
-    },
+    }
 ]
 
 export function About(){
--- lapushel/src/app/components/music/music.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './music.module.css'
-
-export default function music(){
-    return(
-        <div>hola</div>
-    )
-}
